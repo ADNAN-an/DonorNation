8ee8d3b7653c89bbb0e31aaffff09c3d3829f076
Adnan <adnan.anjar2002@gmail.com>
2023-05-30 11:21:42 +0100

    dropdown done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -96,38 +96,34 @@
            
        <!-- <i class="bi bi-list mobile-nav-toggle"></i>  -->
 
+            @guest
+            <a href="{{ route('login') }}" class="appointment-btn scrollto">
+                <span class="d-none d-md-inline">Nous</span> Rejoindre
+            </a>
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+        
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+            </li>
+        @endguest
+   
 
        
       </nav><!-- .navbar -->
 
-      @guest
-      <a href="{{ route('login') }}" class="appointment-btn scrollto">
-          <span class="d-none d-md-inline">Nous</span> Rejoindre
-      </a>
-  @else
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ms-auto">
-              <li class="nav-item dropdown">
-                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                      {{ Auth::user()->name }}
-                  </a>
-  
-                  <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="{{ route('logout') }}"
-                         onclick="event.preventDefault();
-                                   document.getElementById('logout-form').submit();">
-                          {{ __('Logout') }}
-                      </a>
-  
-                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                          @csrf
-                      </form>
-                  </div>
-              </li>
-          </ul>
-      </div>
-  @endguest
   
   </header><!-- End Header -->
 
